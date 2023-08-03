012fa9d9c984839b624a2b76c503b04cc4a649e5
Op-Pautu <pautuop@gmail.com>
2023-08-03 16:20:14 +0530

    Style home page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image'
 const Footer = () => {
   return (
     <div className={styles.container}>
-       <div>©2023 Rufia. All rights reserved.</div>
-        <div></div>
+      <div className='copyright'>©2023 Rufia. All rights reserved.</div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={25} height={25} className={styles.icon} alt="facebook icon" />
+        <Image src="/2.png" width={25} height={25} className={styles.icon} alt="insta icon" />
+        <Image src="/3.png" width={25} height={25} className={styles.icon} alt="twitter icon" />
+        <Image src="/4.png" width={25} height={25} className={styles.icon} alt="youtube icon" />
+      </div>
     </div>
   )
 }
