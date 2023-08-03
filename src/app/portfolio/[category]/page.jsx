a8fdc6baa357e24a92e1f85eaaf3d7ff586262a7
Op-Pautu <pautuop@gmail.com>
2023-08-03 23:43:09 +0530

    Style single portfolio page

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import styles from './page.module.css'
+import Button from '@/components/Button/button'
+import Image from 'next/image'
 
-const Category = () => {
+const Category = ({params}) => {
   return (
-    <div>Category</div>
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image fill={true}
+            className={styles.img}
+            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+            alt="image"
+            />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image fill={true}
+            className={styles.img}
+            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+            alt="image"
+            />
+        </div>
+      </div>
+    </div>
   )
 }
 
