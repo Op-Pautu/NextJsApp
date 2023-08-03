ce7fe9a10817633897f59a399326e22c6ac04aa5
Op-Pautu <pautuop@gmail.com>
2023-08-03 15:25:33 +0530

    Add styles for navbar and footer

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-
+import styles from './navbar.module.css'
 const links = [
     {
       id: 1,
@@ -37,12 +37,16 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-        <Link href="/">rufia</Link>
-        <div>
+    <div className={styles.container}>
+        <Link href="/" className={styles.logo}>rufia</Link>
+        <div className={styles.links}>
             {links.map(link=> (
-                <Link key={link.id} href={link.url}>{link.title}</Link>
+                <Link key={link.id} 
+                    href={link.url}
+                    className={styles.link}
+                >{link.title}</Link>
             ))}
+            <button className={styles.logout}>Logout</button>
         </div>
     </div>
   )
