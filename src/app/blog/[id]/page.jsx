50a43bc639c47ba022b8167101307e3903bc0889
Op-Pautu <pautuop@gmail.com>
2023-08-04 12:59:06 +0530

    Fetching dummy data for server side

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,13 +1,26 @@
 import Image from 'next/image'
 import styles from "./page.module.css";
 import React from 'react'
+import {notFound} from "next/navigation"
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-store",
+  });
 
-const BlogPost = () => {
+  if (!res.ok) {
+   return notFound()
+  }
+
+  return res.json();
+}
+
+const BlogPost = async({params}) => {
+  const data = await getData(params.id)
   return (
     <div className={styles.container}>
     <div className={styles.top}>
       <div className={styles.info}>
-        <h1 className={styles.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h1>
+        <h1 className={styles.title}> {data.title} </h1>
         <p className={styles.desc}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem beatae optio delectus, eligendi, veniam neque tempore laborum voluptas ducimus vero repudiandae voluptates
         </p>
