6f04bd6c2f458bcae21cab3874c2e3ccb2a57401
Op-Pautu <pautuop@gmail.com>
2023-08-05 14:44:30 +0530

    Project login and register pages

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,21 +4,36 @@ import React from 'react'
 import styles from './page.module.css'
 import useSWR from 'swr'
 import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation';
 
 
 
 const Dashboard = () => {
   const session = useSession()
-  console.log(session)
+  const router = useRouter()
+
   const fetcher = (...args) => fetch(...args).then(res => res.json())
 
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
+  
+  if(session.status === 'loading') {
+    return <p>Loading...</p>
+  }
+  if(session.status === 'unauthenticated') {
+    router?.push('/dashboard/login')
+  }
+  if(session.status === 'authenticated') {
+    return (
+      <div className={styles.container}>Dashboard</div>
+    )
+  }
+
+  // console.log(session)
+
 
  
   
-  return (
-    <div className={styles.container}>Dashboard</div>
-  )
+ 
 }
 
 export default Dashboard
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,9 @@
+"use client";
 import Link from 'next/link'
 import React from 'react'
 import styles from './navbar.module.css'
 import DarkModeToggle from '../DarkModeToggle/DarkModeToggle';
+import { signOut, useSession } from 'next-auth/react';
 const links = [
     {
       id: 1,
@@ -37,6 +39,7 @@ const links = [
   
 
 const Navbar = () => {
+  const session = useSession();
   return (
     <div className={styles.container}>
         <Link href="/" className={styles.logo}>rufia</Link>
@@ -48,7 +51,14 @@ const Navbar = () => {
                     className={styles.link}
                 >{link.title}</Link>
             ))}
-            <button className={styles.logout}>Logout</button>
+            {
+              session.status === 'authenticated' && (
+                <button className={styles.logout} onClick={signOut}>
+                  Logout
+                </button>
+              )
+            }
+            
         </div>
     </div>
   )
