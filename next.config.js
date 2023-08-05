6528f9712b5bebbcd0d18027662ece7584048958
Op-Pautu <pautuop@gmail.com>
2023-08-05 14:10:08 +0530

    Write code for registering

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
     images: {
         domains: ["images.pexels.com"],
       },
+      compiler: {
+        // Enables the styled-components SWC transform
+        styledComponents: true,
+      }
 }
 
 module.exports = nextConfig
