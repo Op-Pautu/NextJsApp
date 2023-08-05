a46a9fa3df7d8f8f0b7d9a1f2cb183d1a95f7381
Op-Pautu <pautuop@gmail.com>
2023-08-05 11:24:14 +0530

    Use 0Auth to link gmail

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,21 +3,13 @@
 import React from 'react'
 import styles from './page.module.css'
 import useSWR from 'swr'
+import { useSession } from 'next-auth/react'
 
-async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    cache: "no-store",
-  });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
 
 const Dashboard = () => {
-  
+  const session = useSession()
+  console.log(session)
   const fetcher = (...args) => fetch(...args).then(res => res.json())
 
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
